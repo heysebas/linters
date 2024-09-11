fe7f261599dd7449aadc53c1db904cd8f9d76098
heysebas <sebazgrisales@gmail.com>
2024-09-10 21:05:38 -0500

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+This module contains a simple function to compute the sum of two numbers.
+"""
+
+def suma(num1, num2):
+    """
+    Calculate the sum of two numbers.
+
+    Args:
+        num1 (int or float): The first number.
+        num2 (int or float): The second number.
+
+    Returns:
+        int or float: The sum of num1 and num2.
+    """
+    return num1 + num2
